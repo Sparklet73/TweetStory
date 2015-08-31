5bba4f958c3724e701f35bb19cee5b928891181d
Sparklet73 <talcy.73@gmail.com>
2015-08-31 12:51:09 +0800

    tweets display area 可以依照tags apply後的結果顯示；把addNewTags.js改名成showTweets.js

--- ajax_showtweets.php
+++ ajax_showtweets.php
@@ -1,27 +1,137 @@
 <?php
+
+ini_set('display_errors', true);
+error_reporting(E_ALL | E_STRICT);
+
 require_once 'config.php';
 
-if(isset($_GET['tweetTags'])) {
+if (isset($_GET['tweetTags'])) {
     $arrTweetList = $_GET['tweetTags'];
 //    $arrResult['rsStat'] = true;
 //    $arrResult['rsTweet'] = $json;
-    var_dump($arrTweetList);
+//    var_dump($arrTweetList);
 } else {
+    $arrResult['rsStat'] = false;
+    $arrResult['rsTweet'] = "The parameter has problem.";
+}
+
+$intTimeW = (int) filter_input(INPUT_GET, 'timew', FILTER_SANITIZE_NUMBER_INT);
+$intKeywordsW = (int) filter_input(INPUT_GET, 'keywordsw', FILTER_SANITIZE_NUMBER_INT);
+$intUsersW = (int) filter_input(INPUT_GET, 'usersw', FILTER_SANITIZE_NUMBER_INT);
+$intNounsW = (int) filter_input(INPUT_GET, 'nounsw', FILTER_SANITIZE_NUMBER_INT);
+
+if (!$intTimeW || !$intKeywordsW || !$intUsersW || !$intNounsW) {
     $arrResult['rsStat'] = false;
     $arrResult['rsTweet'] = "The parameter has problem.";
 }
 
 try {
     $dbh = new PDO("mysql:host=$hostname;dbname=$database;charset=utf8", $dbuser, $dbpass);
-    $dbh ->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    foreach($arrTweetList as $key => $value){
-        
+    $tweetScore = array();
+    $tweetTags = array();
+    foreach ($arrTweetList as $key => $value) {
+        if ($key == "Time") {
+            foreach ($value as $v) {
+                $sql = "SELECT `id` FROM `HKALLzh_main` WHERE `created_at` LIKE '" . $v . "%'";
+                $stmt = $dbh->prepare($sql);
+                $stmt->execute();
+                while ($arrQue = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    if (array_key_exists($arrQue['id'], $tweetScore)) {
+                        $tweetScore[$arrQue['id']] += $intTimeW;
+                        $tweetTags[$arrQue['id']] .= "/" . $v;
+                    } else {
+                        $tweetScore[$arrQue['id']] = $intTimeW;
+                        $tweetTags[$arrQue['id']] = $v;
+                    }
+                }
+            }
+        }
+        if ($key == "Keywords") {
+            foreach ($value as $v) {
+                $sql = "SELECT `id` FROM `HKALLzh_main` WHERE `text` LIKE '%" . $v . "%'";
+                $stmt = $dbh->prepare($sql);
+                $stmt->execute();
+                while ($arrQue = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    if (array_key_exists($arrQue['id'], $tweetScore)) {
+                        $tweetScore[$arrQue['id']] += $intKeywordsW;
+                        $tweetTags[$arrQue['id']] .= "/" . $v;
+                    } else {
+                        $tweetScore[$arrQue['id']] = $intKeywordsW;
+                        $tweetTags[$arrQue['id']] = $v;
+                    }
+                }
+            }
+        }
+        if ($key == "Users") {
+            foreach ($value as $v) {
+                $sql = "SELECT `id` FROM `HKALLzh_main` WHERE `from_user_name` LIKE '" . $v . "'";
+                $stmt = $dbh->prepare($sql);
+                $stmt->execute();
+                while ($arrQue = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    if (array_key_exists($arrQue['id'], $tweetScore)) {
+                        $tweetScore[$arrQue['id']] += $intUsersW;
+                        $tweetTags[$arrQue['id']] .= "/" . $v;
+                    } else {
+                        $tweetScore[$arrQue['id']] = $intUsersW;
+                        $tweetTags[$arrQue['id']] = $v;
+                    }
+                }
+            }
+        }
+        if ($key == "Nouns") {
+            foreach ($value as $v) {
+                $sql = "SELECT `id` FROM `HKALLzh_main` WHERE `text` LIKE '%" . $v . "%'";
+                $stmt = $dbh->prepare($sql);
+                $stmt->execute();
+                while ($arrQue = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    if (array_key_exists($arrQue['id'], $tweetScore)) {
+                        $tweetScore[$arrQue['id']] += $intNounsW;
+                        $tweetTags[$arrQue['id']] .= "/" . $v;
+                    } else {
+                        $tweetScore[$arrQue['id']] = $intNounsW;
+                        $tweetTags[$arrQue['id']] = $v;
+                    }
+                }
+            }
+        }
     }
+    arsort($tweetScore);
     
-} catch (pDOException $ex) {
+    $arrResult['rsStat'] = true;
+    $arrResult['rsTweet'] = array();
+    $i = 0;
+//    $onethouTweets == $arrResult['rsTweet'];
+    foreach ($tweetScore as $k => $v) {
+        if ($i < 1000) {
+            $arrResult['rsTweet'][$k]["tags"] = $tweetTags[$k];
+        }
+        $i += 1;
+    }
 
+    $sql = "SELECT `id`, `from_user_name`, `created_at`, `text`, `retweet_count` 
+            FROM `HKALLzh_main` 
+            WHERE `id` IN (" . implode(',', array_keys($arrResult['rsTweet'])) . ")";
+
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+    while ($arrQue = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $arrResult['rsTweet'][$arrQue["id"]]["from_user_name"] = $arrQue['from_user_name'];
+        $arrResult['rsTweet'][$arrQue["id"]]["created_at"] = $arrQue['created_at'];
+        $arrResult['rsTweet'][$arrQue["id"]]["text"] = $arrQue['text'];
+        $arrResult['rsTweet'][$arrQue["id"]]["retweet_cnt"] = $arrQue['retweet_count'];
+    }
+        
+} catch (PDOException $ex) {
+    $arrResult['rsStat'] = false;
+    $arrResult['rsTweet'] = $ex->getMessage();
 } catch (Exception $exc) {
     echo $exc->getMessage();
 }
-//echo json_encode($arrResult);
+
+unset($dbh);
+
+header("Content-type: application/json; charset=utf-8");
+
+echo json_encode($arrResult);
--- showTweets.js
+++ showTweets.js
@@ -1,11 +1,11 @@
-function parseTags(aryLists) {
+function parseTags(aryLists, tv, kv, uv, nv) {
     var jObject = {};
     var arrTime = [];
     var arrKeywords = [];
     var arrUsers = [];
     var arrNouns = [];
     for (var s in aryLists) {
-        console.log(aryLists[s]);
+//        console.log(aryLists[s]);
         ps = aryLists[s].split('|');
         switch (ps[0]) {
             case "Time":
@@ -22,16 +22,16 @@ function parseTags(aryLists) {
                 break;
         }
     }
-    if(arrTime.length > 0) {
+    if (arrTime.length > 0) {
         jObject["Time"] = arrTime;
     }
-    if(arrKeywords.length > 0) {
+    if (arrKeywords.length > 0) {
         jObject["Keywords"] = arrKeywords;
     }
-    if(arrUsers.length > 0) {
+    if (arrUsers.length > 0) {
         jObject["Users"] = arrUsers;
     }
-    if(arrNouns.length > 0) {
+    if (arrNouns.length > 0) {
         jObject["Nouns"] = arrNouns;
     }
 
@@ -40,26 +40,38 @@ function parseTags(aryLists) {
     });
 
     var jqxhr = $.getJSON('ajax_showtweets.php', {
-        tweetTags: jObject
+        tweetTags: jObject,
+        timew: tv,
+        keywordsw: kv,
+        usersw: uv,
+        nounsw: nv
     });
 
     jqxhr.done(function (data) {
         if (data.rsStat) {
-            $('#tweetsDisplay').append(makeTweetContent());
+            var arr = [];
+            $.each(data.rsTweet, function (index, val) {
+                $('#tweetsDisplay').append(makeTweetContent(index, val.created_at, val.from_user_name, val.text, val.retweet_cnt, val.tags));
+            });
+            NProgress.done();
         } else {
             showMessage('danger', data.rsTweet);
         }
     });
 }
 
-function makeTweetContent(tid, time, user, content, rtcount) {
+function makeTweetContent(tid, time, user, content, rtcount, tags) {
     var rtn_content = '';
     rtn_content += '<div class="panel panel-info"><p class="tweet_user">';
     rtn_content += user + '</p><p class="tweet_time"> ' + time + ' </p><br>';
     rtn_content += '<p class="tweet">' + content + '</p>';
-    rtn_content += '<p class="rtcnt">' + rtcount + '</p>';
-    rtn_content += '<a href="#" id="comments">Insert memo!</a>';
+    rtn_content += '<p class="rtcnt">Retweet count: ' + rtcount + '</p>';
     rtn_content += '<a class="btn icon-btn btn-collect" href="#" id="cl-' + tid + '">';
-    rtn_content += '<span class="glyphicon btn-glyphicon glyphicon-plus img-circle text-collect"></span>Collect</a></div>';
+    rtn_content += '<span class="glyphicon btn-glyphicon glyphicon-plus img-circle text-collect"></span>Collect</a>';
+    var tagList = tags.split('/');
+    for (var t in tagList) {
+        rtn_content += '<span class="label greyTag" style="margin-left:2px;margin-right:2px;">' + tagList[t] + '</span>';
+    }
+    rtn_content += '</div>';
     return rtn_content;
 }
