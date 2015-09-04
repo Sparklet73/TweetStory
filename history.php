1bccb584a4959d11c9bae64219cf1688c4a439dc
Sparklet73 <talcy.73@gmail.com>
2015-09-04 13:30:30 +0800

    加入History功能(介面+互動皆完成)

---
+++ history.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>History</title>
+        <meta charset="utf-8">
+        <script src="jquery/jquery-2.1.3.min.js"></script>
+        <script src="jquery/jquery-ui.min.js"></script>
+        <link href="bootstrap-3.3.1-dist/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <script src="bootstrap-3.3.1-dist/dist/js/bootstrap.min.js"></script>
+        <script src="bootstrap-slider/bootstrap-slider.min.js"></script>
+        <link rel="stylesheet" href="bootstrap-slider/bootstrap-slider.min.css" type="text/css" />
+        <script src="returnHistory.js"></script>
+        <style type="text/css">
+            body, html {
+                font-family: "Trebuchet MS Black", "LiHei Pro", "Microsoft JhengHei";
+                /*overflow: hidden;*/
+            }
+            .history {
+                overflow-x: scroll;
+                overflow-y: hidden;
+                white-space: nowrap;
+            }
+            .history_item {
+                white-space: initial;
+                display: inline-block;
+                width: 300px;
+                margin: 10px;
+                box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.26);
+                vertical-align: top;
+            }
+            //----4 tags color (list-group) 設定 開始-----
+            .well {
+                width: inherit;
+            }
+            .timeTag{
+                background: #FFC600;
+            }
+            .keywordsTag{
+                background: #A6DE38;
+            }
+            .usersTag{
+                background: #248E8E;
+            }
+            .nounsTag{
+                background: #ED3C3C;
+            }
+            //----4 tags color (list-group) 設定 結束-----
+        </style>
+    </head>
+    <body>
+        <div class="mywindow" style="margin:0 auto;">
+            <nav class="navbar navbar-inverse">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">TweetStory</a>
+                    </div>
+
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li><a href="browsing.php">Browsing Room<span class="sr-only">(current)</span></a></li>
+                            <li><a href="materials.php">Materials Room</a></li>
+                            <li class="active"><a href="#">History</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <p class="navbar-text" style="font-size: 16px;">Dataset: HKALLzh --- 497,519 tweets (from 2014-08-24 22:06:20 to 2014-12-17 13:55:22)</p>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div> <!-- /.container-fluid -->
+            </nav>
+            <div class="col-md-12">
+                <h4>History</h4>
+                <div class="history" id="history">
+                </div>
+                <script>
+                    returnHistory(0);
+                </script>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ returnHistory.js
@@ -0,0 +1,63 @@
+function returnHistory(userID) {
+
+    $.ajaxSetup({
+        cache: false
+    });
+
+    var jqxhr = $.getJSON('ajax_returnHistory.php', {
+        uID: userID
+    });
+
+    jqxhr.done(function (data) {
+        if (data.rsStat) {
+            $.each(data.rsHistory, function (index, val) {
+                console.log(val.tweets);
+                $('#history').append(makeHistoryContent(index, val.applied_at, val.time, val.time_w, val.keywords, val.keywords_w, val.users, val.users_w, val.nouns, val.nouns_w, val.tweets));
+            });
+        } else {
+            showMessage('danger', data.rsHistory);
+        }
+    });
+}
+
+function makeHistoryContent(hid, applied_at, time, time_w, keywords, keywords_w, users, users_w, nouns, nouns_w, tweets) {
+    var rtn_content = '';
+    rtn_content += '<div class="history_item" id="hs_';
+    rtn_content += hid + '"><h5 style="text-align: right;">';
+    rtn_content += applied_at + '</h5><ul class="list-group"><li class="list-group-item" style="height: 50px;><h5 class="list-group-item-heading">Time:';
+    rtn_content += time_w + '</h5><p class="list-group-item-text" style="text-align: right;">';
+    var word = time.split('|');
+    for (var w in word) {
+        rtn_content += '<span class="label timeTag" style="margin-left: 3px;">';
+        rtn_content += word[w] + '</span>';
+    }
+    rtn_content += '</p></li><li class="list-group-item" style="height: 50px;><h5 class="list-group-item-heading">Keywords:';
+    rtn_content += keywords_w + '</h5><p class="list-group-item-text" style="text-align: right;">';
+    var word = keywords.split('|');
+    for (var w in word) {
+        rtn_content += '<span class="label keywordsTag" style="margin-left: 3px;">';
+        rtn_content += word[w] + '</span>';
+    }
+    rtn_content += '</p></li><li class="list-group-item" style="height: 50px;><h5 class="list-group-item-heading">Users:';
+    rtn_content += users_w + '</h5><p class="list-group-item-text" style="text-align: right;">';
+    var word = users.split('|');
+    for (var w in word) {
+        rtn_content += '<span class="label usersTag" style="margin-left: 3px;">';
+        rtn_content += word[w] + '</span>';
+    }
+    rtn_content += '</p></li><li class="list-group-item" style="height: 50px;><h5 class="list-group-item-heading">Nouns:';
+    rtn_content += nouns_w + '</h5><p class="list-group-item-text" style="text-align: right;">';
+    var word = nouns.split('|');
+    for (var w in word) {
+        rtn_content += '<span class="label nounsTag" style="margin-left: 3px;">';
+        rtn_content += word[w] + '</span>';
+    }
+    rtn_content += '</p></li></ul><div class="row" style="overflow-x: hidden; overflow-y:auto; height: 280px;"><div class="well" id="well2">';
+    rtn_content += '<div class="list-group">';
+    for (var t in tweets) {
+        rtn_content += '<li href="#" class="list-group-item" style="padding:10px 15px;"><p class="list-group-item-text">';
+        rtn_content += tweets[t]['text'] +'</p></li>';
+    }
+    rtn_content += '</li></div></div></div></div>';
+    return rtn_content;
+}
